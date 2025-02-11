b12a25d76632b0cc64f74e1c64b1da795064ed64
Ryanxue08 <ryan.xue@education.nsw.gov.au>
2025-02-11 10:30:41 +0000

    Finshed Conditionals video

--- compare.py
+++ compare.py
@@ -1,9 +1,7 @@
 x = int(input('What is x? '))
 y = int(input('What is y? '))
 
-if x < y:
-    print('x is less than y')
-if x > y:
-    print('x is greater than y')
 if x == y:
-    print ('x is equal to y')
+    print('x is equal to y')
+else:
+    print('x is not equal to y')
