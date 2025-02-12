5199c7edaa5a702ff75a2f6b0fc0a8c74717439d
Ryanxue08 <ryan.xue@education.nsw.gov.au>
2025-02-12 03:01:05 +0000

    Complted bank.py

---
+++ 1-Conditionals/bank/bank.py
@@ -0,0 +1,12 @@
+def bank_greeting():
+    greeting = input('Greetings: ').strip().lower()
+
+    if greeting.startswith('hello'):
+        print('$0')
+    elif greeting.startswith('h'):
+        print('$20')
+    else:
+        print('$100')
+
+
+bank_greeting()
